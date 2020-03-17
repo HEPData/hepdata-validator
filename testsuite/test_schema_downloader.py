67b43c10ab5330ca522efdbfb33ef37d7d902ee3
Sinclert Pérez <Sinclert@hotmail.com>
2020-03-17 14:45:30 -0400

    Define HTTPSchemaDownloader tests

---
+++ testsuite/test_schema_downloader.py
@@ -0,0 +1,143 @@
+import os
+import pytest
+import shutil as sh
+from hepdata_validator.schema_downloader import HTTPSchemaDownloader
+from requests.exceptions import HTTPError
+from unittest.mock import patch
+
+
+####################################################
+#                 Tests fixtures                   #
+####################################################
+
+
+@pytest.fixture(scope="module")
+def http_downloader():
+    """
+    Generates a valid HTTPSchemaDownloader using example names
+    """
+
+    return HTTPSchemaDownloader(
+        endpoint="https://testing.com/schemas/1.0.0",
+        company="testing.com",
+        version="1.0.0",
+    )
+
+
+####################################################
+#                   Tests mocks                    #
+####################################################
+
+
+class MockedResponse(object):
+
+    def __init__(self, content, http_code):
+        self.content = content
+        self.http_code = http_code
+
+    def raise_for_status(self):
+        if self.http_code != 200:
+            raise HTTPError
+
+    @property
+    def text(self):
+        return self.content
+
+
+def get_patched_valid_response(url):
+    return MockedResponse('{"field_1": "value_1", "field_2": "value_2"}', 200)
+
+
+def get_patched_invalid_response(url):
+    return MockedResponse("Not found", 404)
+
+
+####################################################
+#            HTTPSchemaDownloader tests            #
+####################################################
+
+
+@patch('requests.get', new=get_patched_valid_response)
+def test_http_downloader_get_schema(http_downloader):
+    """
+    Tests the HTTPSchemaDownloader with a real schema name
+    :param http_downloader: HTTPSchemaDownloader
+    """
+
+    file_name = "real_schema.json"
+
+    schema_spec = http_downloader.get_schema(file_name)
+    assert len(schema_spec) > 0
+
+
+@patch('requests.get', new=get_patched_invalid_response)
+def test_http_downloader_get_missing_schema(http_downloader):
+    """
+    Tests the HTTPSchemaDownloader with a missing schema name
+    :param http_downloader: HTTPSchemaDownloader
+    """
+
+    file_name = "missing_schema.json"
+
+    with pytest.raises(HTTPError):
+        http_downloader.get_schema(file_name)
+
+
+def test_http_downloader_save_schema(http_downloader):
+    """
+    Tests the HTTPSchemaDownloader with an invalid initialization
+    :param http_downloader: HTTPSchemaDownloader
+    """
+
+    schema_name = "dummy.json"
+    schema_spec = '{"key_1": "value_1", "key_2": "value_2"}'
+
+    http_downloader.save_locally(schema_name, schema_spec, overwrite=True)
+
+    expected_folder = http_downloader.saved_schema_path
+    expected_path = os.path.join(expected_folder, schema_name)
+
+    assert os.path.isfile(expected_path)
+
+
+def test_http_downloader_save_existing_schema(http_downloader):
+    """
+    Tests the HTTPSchemaDownloader with an invalid initialization
+    :param http_downloader: HTTPSchemaDownloader
+    """
+
+    schema_name = "dummy.json"
+    schema_spec_1 = '{"key_1": "value_1", "key_2": "value_2"}'
+    schema_spec_2 = '{"key_1": "new_value_1", "key_2": "new_value_2"}'
+
+    http_downloader.save_locally(schema_name, schema_spec_1, overwrite=True)
+    http_downloader.save_locally(schema_name, schema_spec_2, overwrite=False)
+
+    expected_folder = http_downloader.saved_schema_path
+    expected_path = os.path.join(expected_folder, schema_name)
+
+    with open(expected_path, 'r') as f:
+        file_content = f.read()
+
+    assert file_content == schema_spec_1
+
+
+def test_http_downloader_invalid_schema_folder(http_downloader):
+    """
+    Tests the HTTPSchemaDownloader with an invalid folder
+    :param http_downloader: HTTPSchemaDownloader
+    """
+
+    expected_folder = http_downloader.saved_schema_path
+
+    # Create folder with restrictive permissions
+    sh.rmtree(expected_folder)
+    os.makedirs(expected_folder, mode=0o400)
+
+    schema_name = "dummy.json"
+    schema_spec = '{"key_1": "value_1", "key_2": "value_2"}'
+
+    with pytest.raises(OSError):
+        http_downloader.save_locally(schema_name, schema_spec, overwrite=True)
+
+    sh.rmtree(expected_folder)
